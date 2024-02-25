e91cf03ed13b8ecad597115fe73c8c610da55ca2
Tobias Dély <cleverhatcamouflage@gmail.com>
2024-02-25 17:06:36 +0100

    feat(seccomp): -d:noseccomp for valgrind

--- inosync.nim
+++ inosync.nim
@@ -1,7 +1,6 @@
 import std / [inotify, intsets, parseopt, paths, posix, strutils, tables,
               tempfiles]
 import std/private/osfiles
-import seccomp
 
 {.passL: "`pkg-config --libs poppler-glib`".}
 when defined(useFuthark) or defined(useFutharkForPoppler):
@@ -44,43 +43,45 @@ var errno {.importc, header: "<errno.h>".}: int
 
 proc parentDir(path: string): string = string(parentDir(Path(path)))
 
-proc lockdown() =
-  ## Allow only a subset of syscalls.
-  let ctx = seccomp_ctx()
-  ctx.add_rule(Allow, "read")
-  ctx.add_rule(Allow, "write")
-  ctx.add_rule(Allow, "close")
-  ctx.add_rule(Allow, "rename")
-  ctx.add_rule(Allow, "unlink")
-  ctx.add_rule(Allow, "newfstatat")
-  ctx.add_rule(Allow, "exit_group")
-  ctx.add_rule(Allow, "inotify_add_watch")
-  ctx.add_rule(Allow, "inotify_rm_watch")
-  ctx.add_rule(Allow, "openat")
-  ctx.add_rule(Allow, "lseek")
-  ctx.add_rule(Allow, "getrandom")
-  ctx.add_rule(Allow, "fcntl")
-  ctx.add_rule(Allow, "rt_sigaction")
-  ctx.add_rule(Allow, "rt_sigprocmask")
-  ctx.add_rule(Allow, "rt_sigreturn")
-  ctx.add_rule(Allow, "tkill")
-  # Poppler
-  ctx.add_rule(Allow, "fstat")
-  ctx.add_rule(Allow, "brk")
-  ctx.add_rule(Allow, "mmap")
-  ctx.add_rule(Allow, "munmap")
-  ctx.add_rule(Allow, "mremap")
-  ctx.add_rule(Allow, "mprotect")
-  ctx.add_rule(Allow, "getdents64")
-  ctx.add_rule(Allow, "pread64")
-  ctx.add_rule(Allow, "futex")
-  # Alpine Linux
-  ctx.add_rule(Allow, "writev")
-  ctx.add_rule(Allow, "readv")
-  ctx.add_rule(Allow, "open")
-  ctx.add_rule(Allow, "ioctl")
-  ctx.add_rule(Allow, "lstat")
-  ctx.load()
+when not defined(noseccomp):
+  import seccomp
+  proc lockdown() =
+    ## Allow only a subset of syscalls.
+    let ctx = seccomp_ctx()
+    ctx.add_rule(Allow, "read")
+    ctx.add_rule(Allow, "write")
+    ctx.add_rule(Allow, "close")
+    ctx.add_rule(Allow, "rename")
+    ctx.add_rule(Allow, "unlink")
+    ctx.add_rule(Allow, "newfstatat")
+    ctx.add_rule(Allow, "exit_group")
+    ctx.add_rule(Allow, "inotify_add_watch")
+    ctx.add_rule(Allow, "inotify_rm_watch")
+    ctx.add_rule(Allow, "openat")
+    ctx.add_rule(Allow, "lseek")
+    ctx.add_rule(Allow, "getrandom")
+    ctx.add_rule(Allow, "fcntl")
+    ctx.add_rule(Allow, "rt_sigaction")
+    ctx.add_rule(Allow, "rt_sigprocmask")
+    ctx.add_rule(Allow, "rt_sigreturn")
+    ctx.add_rule(Allow, "tkill")
+    # Poppler
+    ctx.add_rule(Allow, "fstat")
+    ctx.add_rule(Allow, "brk")
+    ctx.add_rule(Allow, "mmap")
+    ctx.add_rule(Allow, "munmap")
+    ctx.add_rule(Allow, "mremap")
+    ctx.add_rule(Allow, "mprotect")
+    ctx.add_rule(Allow, "getdents64")
+    ctx.add_rule(Allow, "pread64")
+    ctx.add_rule(Allow, "futex")
+    # Alpine Linux
+    ctx.add_rule(Allow, "writev")
+    ctx.add_rule(Allow, "readv")
+    ctx.add_rule(Allow, "open")
+    ctx.add_rule(Allow, "ioctl")
+    ctx.add_rule(Allow, "lstat")
+    ctx.load()
 
 proc toString(event: uint32): string =
   case event
@@ -385,7 +386,8 @@ proc main() =
   `=destroy`(argsets)
   debug $wl
 
-  lockdown()
+  when not defined(noseccomp):
+    lockdown()
   watch wl
   for i in 0..wl.pairs.high:
     # Process toggle-types even if pair is incomplete
--- inosync.nimble
+++ inosync.nimble
@@ -11,13 +11,13 @@ requires "seccomp >= 0.2.1"
 
 # Tasks
 task debug, "Create a debug build":
-  exec "nim c --lineDir:on --debuginfo:on --debugger:native -d:useMalloc inosync.nim"
+  exec "nim c --lineDir:on --debuginfo:on --debugger:native -d:useMalloc -d:noseccomp inosync.nim"
 
 task futhark, "Build/rebuild futhark wrappers":
   exec "nim c -d:useFuthark -d:futharkRebuild --maxLoopIterationsVM:50000000 --cc:clang inosync.nim"
 
 task build, "Create a development build":
-  exec "nim c inosync.nim"
+  exec "nim c -d:noseccomp inosync.nim"
 
 task release, "Build for release":
   exec "nim c -d:release -d:danger --opt:speed -d:useMalloc inosync.nim"
